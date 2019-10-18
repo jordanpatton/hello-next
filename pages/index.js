772030658b846e5a35fb836dfb32d73bbb24cd0b
Jordan Patton <jordanpatton@gmail.com>
2019-10-17 17:58:20 -0700

    end basic lessons

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,45 @@
-import fetch from 'isomorphic-unfetch';
 import Link from 'next/link';
 
 import Layout from '../components/MyLayout';
 
-const Index = props => (
-    <Layout>
-        <h1>Batman TV Shows</h1>
-        <ul>
-            {props.shows.map(show => (
-                <li key={show.id}>
-                    <Link href="/p/[id]" as={`/p/${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </Layout>
+function getPosts() {
+    return [
+        { id: 'hello-nextjs', title: 'Hello Next.js' },
+        { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+        { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' },
+    ];
+}
+
+const PostLink = ({ post }) => (
+    <li>
+        <Link href="/p/[id]" as={`/p/${post.id}`}>
+            <a>{post.title}</a>
+        </Link>
+        <style jsx>
+            {`
+                a { color: blue; font-family: 'Arial'; text-decoration: none; }
+                a:hover { opacity: 0.6; }
+                li { list-style: none; margin: 5px 0; }
+            `}
+        </style>
+    </li>
 );
 
-Index.getInitialProps = async function() {
-    const r = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-    const rj = await r.json();
-    console.log(`Show data fetched. Count: ${rj.length}`);
-    return { shows: rj.map(entry => entry.show) };
-};
-
-export default Index;
+export default function Blog() {
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                {getPosts().map(post => (
+                    <PostLink key={post.id} post={post} />
+                ))}
+            </ul>
+            <style jsx>
+                {`
+                    h1 { font-family: 'Arial'; }
+                    ul { padding: 0; }
+                `}
+            </style>
+        </Layout>
+    );
+}
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,21 +1,32 @@
-import fetch from 'isomorphic-unfetch';
+import { useRouter } from 'next/router';
+import Markdown from 'react-markdown';
 
 import Layout from '../../components/MyLayout';
 
-const Post = props => (
-    <Layout>
-        <h1>{props.show.name}</h1>
-        <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-        <img src={props.show.image.medium} />
-    </Layout>
-);
+export default () => {
+    const router = useRouter();
+    return (
+        <Layout>
+            <h1>{router.query.id}</h1>
+            <div className="markdown">
+                <Markdown source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
 
-Post.getInitialProps = async function(context) {
-    const { id } = context.query;
-    const r = await fetch(`https://api.tvmaze.com/shows/${id}`);
-    const show = await r.json();
-    console.log(`Fetched show: ${show.name}`);
-    return { show };
+### This is a title
+
+And here's the content.
+                `} />
+            </div>
+            <style global jsx>
+                {`
+                    .markdown { font-family: 'Arial'; }
+                    .markdown a { color: blue; text-decoration: none; }
+                    .markdown a:hover { opacity: 0.6; }
+                    .markdown h3 { margin: 0; padding: 0; text-transform: uppercase; }
+                `}
+            </style>
+        </Layout>
+    );
 };
-
-export default Post;
